00ee1a2d18f49be8b63b8c02f7900c45b940bfab
Arya More <aryamore405@gmail.com>
2026-07-05 13:53:38 +0530

    Done some minor cahnges

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 # This is python file for hello
-import pandas as pandas
+import pandas as pd
 import sklearn
-import numpy as numpy
+import numpy as np
 print("Hello World")
 print("End")
 print("Thanks")
 print("End")
+print("The end")
+print("End")
+print("The end")
+print("ok")
